688dcc22560f53f6f79ef2ccd1e008b7ee85683a
Abigail Watson <awatson1978@gmail.com>
2016-06-13 15:52:24 -0500

    fhir-demo

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-patient',
-  version: '1.2.0',
+  version: '1.3.0',
   summary: 'HL7 FHIR Patient Resource',
   git: 'https://github.com/clinical-meteor/hl7-resource-patient',
   documentation: 'README.md'
@@ -21,26 +21,26 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:extended-api@2.2.2');
-  api.use('clinical:router@2.0.17');
+  // api.use('clinical:router@2.0.17');
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.4.0');
+  api.use('clinical:hl7-resource-datatypes@0.4.2');
 
-  api.addFiles('client/components/patientUpsertPage/patientUpsertPage.html', ['client']);
-  api.addFiles('client/components/patientUpsertPage/patientUpsertPage.js', ['client']);
-  api.addFiles('client/components/patientUpsertPage/patientUpsertPage.less', ['client']);
-
-  api.addFiles('client/components/patientsTablePage/patientsTablePage.html', ['client']);
-  api.addFiles('client/components/patientsTablePage/patientsTablePage.js', ['client']);
-  api.addFiles('client/components/patientsTablePage/patientsTablePage.less', ['client']);
-  api.addFiles('client/components/patientsTablePage/jquery.tablesorter.js', ['client']);
-
-  api.addFiles('client/components/patientPreviewPage/patientPreviewPage.html', ['client']);
-  api.addFiles('client/components/patientPreviewPage/patientPreviewPage.js', ['client']);
-  api.addFiles('client/components/patientPreviewPage/patientPreviewPage.less', ['client']);
-
-  api.addFiles('client/components/patientsListPage/patientsListPage.html', ['client']);
-  api.addFiles('client/components/patientsListPage/patientsListPage.js', ['client']);
-  api.addFiles('client/components/patientsListPage/patientsListPage.less', ['client']);
+  // api.addFiles('client/components/patientUpsertPage/patientUpsertPage.html', ['client']);
+  // api.addFiles('client/components/patientUpsertPage/patientUpsertPage.js', ['client']);
+  // api.addFiles('client/components/patientUpsertPage/patientUpsertPage.less', ['client']);
+  //
+  // api.addFiles('client/components/patientsTablePage/patientsTablePage.html', ['client']);
+  // api.addFiles('client/components/patientsTablePage/patientsTablePage.js', ['client']);
+  // api.addFiles('client/components/patientsTablePage/patientsTablePage.less', ['client']);
+  // api.addFiles('client/components/patientsTablePage/jquery.tablesorter.js', ['client']);
+  //
+  // api.addFiles('client/components/patientPreviewPage/patientPreviewPage.html', ['client']);
+  // api.addFiles('client/components/patientPreviewPage/patientPreviewPage.js', ['client']);
+  // api.addFiles('client/components/patientPreviewPage/patientPreviewPage.less', ['client']);
+  //
+  // api.addFiles('client/components/patientsListPage/patientsListPage.html', ['client']);
+  // api.addFiles('client/components/patientsListPage/patientsListPage.js', ['client']);
+  // api.addFiles('client/components/patientsListPage/patientsListPage.less', ['client']);
 
   //api.addFiles('server/methods.js', 'server');
 
