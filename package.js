7cd856843ece8f8e6a00c8206b90c717a9b6b85f
Abigail Watson <awatson1978@gmail.com>
2016-12-18 02:15:37 -0600

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-patient',
-  version: '1.5.11',
+  version: '1.5.12',
   summary: 'HL7 FHIR Resource - Patient',
   git: 'https://github.com/clinical-meteor/hl7-resource-patient',
   documentation: 'README.md'
