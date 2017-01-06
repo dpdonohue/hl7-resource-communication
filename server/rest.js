0b261330eaf39e88266a13b5d5ac9f474a980431
Abigail Watson <awatson1978@gmail.com>
2017-01-06 00:37:05 -0600

    post /fhir/Patient/_search?_count=1

--- server/rest.js
+++ server/rest.js
@@ -21,30 +21,14 @@ JsonRoutes.add("get", "/fhir/Patient/:id", function (req, res, next) {
       process.env.TRACE && console.log('accessToken.userId', accessToken.userId);
     }
 
-    if (typeof SiteStatistics === "object") {
-      SiteStatistics.update({_id: "configuration"}, {$inc:{
-        "Patients.count.read": 1
-      }});
-    }
+    // if (typeof SiteStatistics === "object") {
+    //   SiteStatistics.update({_id: "configuration"}, {$inc:{
+    //     "Patients.count.read": 1
+    //   }});
+    // }
 
-    var patientData;
-    var id = req.params.id;
-
-    if (id === '_search') {
-      var searchLimit = 1;
-      if (req && req.query && req.query._count) {
-        searchLimit = parseInt(req.query._count);
-      }
-      patientData = Patients.find({}, {limit: searchLimit}).map(function(patient){
-        patient.id = patient._id;
-        delete patient._document;
-        delete patient._id;
-        return patient;
-      });
-    } else {
-      patientData = Patients.findOne({_id: id});
-      delete patientData._document;
-    }
+    var patientData = Patients.findOne({_id: req.params.id});
+    delete patientData._document;
 
     process.env.TRACE && console.log('patientData', patientData);
 
@@ -144,6 +128,49 @@ JsonRoutes.add("get", "/fhir/Patient", function (req, res, next) {
   }
 });
 
+
+JsonRoutes.add("post", "/fhir/Patient/:id", function (req, res, next) {
+  process.env.DEBUG && console.log('GET /fhir/Patient/' + JSON.stringify(req.query));
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  var accessTokenStr = (req.params && req.params.access_token) || (req.query && req.query.access_token);
+  var accessToken = oAuth2Server.collections.accessToken.findOne({accessToken: accessTokenStr});
+
+  if (accessToken || process.env.NOAUTH || Meteor.settings.private.disableOauth) {
+
+    if (accessToken) {
+      process.env.TRACE && console.log('accessToken', accessToken);
+      process.env.TRACE && console.log('accessToken.userId', accessToken.userId);
+    }
+
+    var patientData;
+
+    if (req.params.id === '_search') {
+      var searchLimit = 1;
+      if (req && req.query && req.query._count) {
+        searchLimit = parseInt(req.query._count);
+      }
+      patientData = Patients.find({}, {limit: searchLimit}).map(function(patient){
+        patient.id = patient._id;
+        delete patient._document;
+        delete patient._id;
+        return patient;
+      });
+    }
+
+    process.env.TRACE && console.log('patientData', patientData);
+
+    JsonRoutes.sendResult(res, {
+      code: 200,
+      data: patientData
+    });
+  } else {
+    JsonRoutes.sendResult(res, {
+      code: 401
+    });
+  }
+});
 // WebApp.connectHandlers.use("/fhir/Patient", function(req, res, next) {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
 //   return next();
