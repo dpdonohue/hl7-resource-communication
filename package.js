fded7d221f777784f1740e837fc2b95a08bc050e
Abigail Watson <awatson1978@gmail.com>
2017-01-06 12:00:05 -0600

    Bundle.total

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-patient',
-  version: '1.6.6',
+  version: '1.6.7',
   summary: 'HL7 FHIR Resource - Patient',
   git: 'https://github.com/clinical-meteor/hl7-resource-patient',
   documentation: 'README.md'
