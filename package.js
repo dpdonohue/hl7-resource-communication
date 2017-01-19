9afed2aed923b036c458f3e55ee325972a3930cd
Abigail Watson <awatson1978@gmail.com>
2017-01-19 16:10:25 -0600

    Meteor.publish()

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-patient',
-  version: '1.6.44',
+  version: '1.6.45',
   summary: 'HL7 FHIR Resource - Patient',
   git: 'https://github.com/clinical-meteor/hl7-resource-patient',
   documentation: 'README.md'
